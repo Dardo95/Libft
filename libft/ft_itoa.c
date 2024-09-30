1b8b9b82e4a3e62758333b8cf500dcb9b103baa4
root <root@DESKTOP-NP4I4GD>
2024-09-30 10:21:24 +0200

    itoa

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -9,18 +9,79 @@
 /*   Updated: 2024/09/28 18:31:54 by enogueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
-char *ft_itoa(int n)
-{
-	char sign;
 
+
+#include "libft.h"
+
+static int	ft_count_num(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n == -2147483648)
+		return (10);
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		n = -n;
+	while (n > 0)
+	{
+		n = n / 10;
+		count++;
+	}
+	return (count);
+}
+
+char    *ft_itoa(int n)
+{
+	char	*str;
+	int	len;
+	long	nb;
+
+	nb = (long)n;
+	len = ft_count_num(n);
+	if (n < 0)
+		len++;
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
 	if (n < 0)
 	{
-		sign = '-';
-		n = -n
+		str[0] = '-';
+		nb = -nb;
 	}
-	if (n > 10)
+	while (nb >= 10)
 	{
-		
+		str[len - 1] = ((nb % 10) + '0');
+		nb = (nb / 10);
+		len --;
 	}
-}*/
+	str[len - 1] = ((nb %10) + '0');
+	return (str);
+}
+
+int main() {
+    // Casos de prueba
+    int test_cases[] = {
+        0,               // Caso 0
+        1,               // Caso positivo simple
+        -1,              // Caso negativo simple
+        42,              // Caso positivo
+        -42,             // Caso negativo
+        123456,          // Caso positivo grande
+        -123456,         // Caso negativo grande
+        2147483647,         // Caso de valor máximo de int
+        -2147483648          // Caso de valor mínimo de int
+    };
+
+    int num_cases = sizeof(test_cases) / sizeof(test_cases[0]);
+
+    for (int i = 0; i < num_cases; i++) {
+        char *result = ft_itoa(test_cases[i]);
+        printf("ft_itoa(%d) = \"%s\"\n", test_cases[i], result);
+        free(result);  // Asegúrate de liberar la memoria
+    }
+
+    return 0;
+}
