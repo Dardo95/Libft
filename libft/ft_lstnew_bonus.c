a043b61ae0750e8d87902e90d87814197edfff34
unknown <dsanto-tomas@hotmail.com>
2024-10-03 12:43:03 +0200

    act

--- libft/ft_lstnew_bonus.c
+++ libft/ft_lstnew_bonus.c
@@ -12,11 +12,14 @@
 
 #include "libft.h"
 
-/*t_list	*ft_lstnew_bonus(void *content)
+t_list	*ft_lstnew_bonus(void *content)
 {
-	t_list	newnode;
+	t_list	*newnode;
 
 	newnode = (t_list)malloc(sizeof(t_list));
 	if (!newmode)
 		return (NULL);
-}*/
+	newnode->content;
+	newnode->next;
+	return (newnode);
+}
