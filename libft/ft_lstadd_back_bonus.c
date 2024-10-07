d2f359bbc6a231534c9e4855ef8f5f90e239268c
root <root@DESKTOP-NP4I4GD>
2024-10-07 09:58:18 +0200

    act

--- libft/ft_lstadd_back_bonus.c
+++ libft/ft_lstadd_back_bonus.c
@@ -6,13 +6,27 @@
 /*   By: ryner <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 01:27:37 by ryner             #+#    #+#             */
-/*   Updated: 2024/10/07 01:28:10 by ryner            ###   ########.fr       */
+/*   Updated: 2024/10/07 09:53:45 by ryner            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_back(t_list **lst, t_list *new)
-{
+//lst: el puntero al primer nodo de una lista.
+//new: el puntero a un nodo que añadir a la lista.
+//Añade el nodo ’new’ al final de la lista ’lst’
 
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*lastnode;
+
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	lastnode = ft_lstlast(*lst);
+	lastnode->next = new;
 }
